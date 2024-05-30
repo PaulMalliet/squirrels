75805a87d8d3189a0ccaa1c03bf6999005a41c23
Paul Malliet <paul.malliet@sciencespo.fr>
2024-05-30 14:17:56 +0000

    https://github.com/PaulMalliet/squirrels.git

---
+++ R/doc_data_demo_squirrels.R
@@ -0,0 +1,12 @@
+#' data_demo_squirrels
+#'
+#' Un jeu de donnee restreint sur les ecureuils
+#'
+#' @format A data frame with 25 rows and 3 variables:
+#' \describe{
+#'   \item{ long }{  numeric }
+#'   \item{ lat }{  numeric }
+#'   \item{ primary_fur_color }{  character }
+#' }
+#' @source ThinkR
+"data_demo_squirrels"
---
+++ R/doc_nyc_squirrels_act.R
@@ -0,0 +1,13 @@
+#' nyc_squirrels_act
+#'
+#' Description.
+#'
+#' @format A data frame with 6 rows and 4 variables:
+#' \describe{
+#'   \item{ age }{  character - l age des ecureuils }
+#'   \item{ primary_fur_color }{  character }
+#'   \item{ activity }{  character }
+#'   \item{ counts }{  numeric }
+#' }
+#' @source Source
+"nyc_squirrels_act"
